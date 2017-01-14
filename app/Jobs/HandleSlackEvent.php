2dd8bae277718ac8f1f169912cbd313e14e829d2
Shalvah <diakon.ng@gmail.com>
2017-01-14 18:01:43 +0100

    Add support for user-defined projects

--- app/Jobs/HandleSlackEvent.php
+++ app/Jobs/HandleSlackEvent.php
@@ -86,13 +86,13 @@ class HandleSlackEvent implements ShouldQueue
         $botUserId = Credential::where('team_id', $this->request['team_id'])->get()->first()->bot_user_id;
         if (preg_match("/<@$botUserId>/", $text)) {
             $matches = [];
-            if (preg_match("/username:\s(\w+)/i", $text, $matches)) {
+            if (preg_match("/username:\s+(\w+)/i", $text, $matches)) {
                  $parsed=array(
                     'type' => 'gitlab-add',
                     'username' => $matches[1]
                 );
-                if (preg_match("/project:\s(\w+-?\w+)/i", $text, $matches)) {
-                    $parsed["project"] = $matches[1];
+                if (preg_match("/project:\s+(\w+-?\w+)/i", $text, $matches)) {
+                    $parsed["project"] = strtolower($matches[1])
                 }
                 else $parsed["project"] = "getting-started";
                 Log::info("Parsed: ".print_r($parsed, true));
