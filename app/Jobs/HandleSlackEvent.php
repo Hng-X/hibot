fc374c6d60554925a6f0062cf2040606117b1683
Shalvah <diakon.ng@gmail.com>
2017-01-14 19:03:20 +0100

    Debugging

--- app/Jobs/HandleSlackEvent.php
+++ app/Jobs/HandleSlackEvent.php
@@ -171,7 +171,7 @@ class HandleSlackEvent implements ShouldQueue
             'access_level' => 30
        );
 
-        curl_setopt($ch, CURLOPT_URL, "http://gitlab.com/api/v3/projects/$projId/members?user_id=$userId&access_level=30");
+        curl_setopt($ch, CURLOPT_URL, "http://gitlab.com/api/v3/projects/$projId/member?user_id=$userId&access_level=30");
 
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
