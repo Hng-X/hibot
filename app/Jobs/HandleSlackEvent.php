cc759d8249c81313214400e16fbd7f4ed7e22dc5
Shalvah <diakon.ng@gmail.com>
2017-01-14 20:43:54 +0100

    Fix bug

--- app/Jobs/HandleSlackEvent.php
+++ app/Jobs/HandleSlackEvent.php
@@ -36,7 +36,10 @@ class HandleSlackEvent implements ShouldQueue
     public function handle()
     {
         if ($this->request['event']['type'] == "message") {
-            $userId=$this->request['event']['user'];
+            if(isset($this->request['event']['user'])) {
+$userId=$this->request['event']['user'];
+}
+else return;
             if (!isset($this->request['event']['subtype'])) {
             $rawText = $this->request['event']['text'];
             $parsedText = $this->parseText($rawText);
