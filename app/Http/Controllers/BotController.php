0b06f268e1389ec49d6e7e0debbc40a8051e0c07
Shalvah <diakon.ng@gmail.com>
2017-01-15 16:02:17 +0100

    Remove unused code

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,10 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Middleware\EventsMiddleware;
 use App\Jobs\HandleSlackEvent;
-use App\Models\Credential;
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class BotController extends Controller
 {
@@ -28,46 +25,6 @@ class BotController extends Controller
         $data = $request->all();
         dispatch(new HandleSlackEvent($data));
         return response('Ok', 200);
-    }
-
-    public function test()
-    {
-        $data = [];
-        $data['text'] = "Hi guys... @channel :blush:";
-        $data['channel'] = "#bot-testing";
-        $data['response_type'] = "saved";
-        $data['team_id'] = "T32HFDCLR";
-
-        $response = $this->respond($data);
-        if ($response['ok'] === true) {
-            return view('Auth/add', ['result' => "OK"]);
-        } else return view('Auth/add', ['result' => $response['error']]);
-    }
-
-    /**
-     * Posts responses to Slack
-     */
-    public function respond(array $data)
-    {
-        //if ($data['response_type'] == 'saved')
-        $client = new Client();
-        $response = $client->request('GET', 'https://slack.com/api/chat.postMessage',
-            ['query' => [
-                'token' => Credential::where('team_id', $data['team_id'])->first()->bot_access_token,
-                'channel' => $data['channel'],
-                'text' => $data['text']
-            ]
-            ]);
-        return json_decode($response->getBody(), true);
-    }
-
-    public function events(Request $request)
-    {
-        if ($request->input("type") == "url_verification") {
-            return response($request->input("challenge"), 200);
-        }
-
-        //return $next($request);
     }
 
 }
