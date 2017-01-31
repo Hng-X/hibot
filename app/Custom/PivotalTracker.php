037bf3914bef8a511683288923202c87685c3c58
Shalvah <diakon.ng@gmail.com>
2017-01-31 09:03:32 +0100

    Add PivotalTracker class

---
+++ app/Custom/PivotalTracker.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Custom;
+
+use App\Slack\SlackMessage;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+
+class PivotalTracker
+{
+
+    public static function addToPivotal($email, $projId = "1961795")
+    {
+        $client = new Client();
+
+        //add user to project
+        $resp = $client->request("POST", "https://pivotaltracker.com/services/v5/projects/$projId/memberships", array("json" => ["email" => $email, "role" => "member"],
+"headers" => ["X-Tracker-Token" => env("PIVOTAL_TRACKER_TOKEN"),]));
+        $resp = json_decode($resp->getBody(), true);
+        Log::info("Resp add: " . print_r($resp, true));
+        return $resp;
+    }
+
+    public static function sendPivotalAddResult($result, array $data, $project="Factory_core")
+    {
+        $team = $data['team_id'];
+        $user = $data['event']['user'];
+        $channel = $data['event']['channel'];
+
+        if ($result != null && $result["role"] == "member") {
+            $text = "Added to Pivotal project: $project! <@$user>";
+            $message = new SlackMessage($team, $channel, $text);
+            return $message->send();
+        } else {
+            $text = "Sorry, I couldn't add you to *$project*, <@$user>. Please double-check your email and try again. ";
+            $message = new SlackMessage($team, $channel, $text);
+            return $message->send();
+        }
+    }
+}
